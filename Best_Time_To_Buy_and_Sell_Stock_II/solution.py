c16de2f82dcf222f71126bc38d4e1193f4a9cecb
MahirR199 <mahir199.tanzil@gmail.com>
2024-09-16 13:18:20 -0400

    Best_time_to_buy_and_sell_stock_II

---
+++ Best_Time_To_Buy_and_Sell_Stock_II/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        i=0
+        lo = prices[0]
+        hi = prices[0]
+        profit = 0
+        n = len(prices)
+        while(i<n-1):
+            #look where to buy
+            while i<n-1 and prices[i]>=prices[i+1]:
+                i+=1
+            lo = prices[i]
+            #look where to sell
+            while i<n-1 and prices[i]<=prices[i+1]:
+                i+=1
+            #calc profit
+            hi = prices[i]
+            profit+=hi-lo
+        return profit
+        
